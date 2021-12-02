e3f3fca2d9bbb14d88b7caf29f12853d4b8f0a50
alexa <alexa@LAPTOP-HOP4Q31V>
2021-12-02 15:54:46 -0500

    Clase 7
    - Clases: Encapsulamiento + sobrecarga
    - creación app-web
    - mini test unitario

---
+++ pruebas/src/main/java/clase7/oop/BuscadorMainClase7.java
@@ -0,0 +1,16 @@
+package clase7.oop;
+
+import clase6oop.Buscador;
+
+public class BuscadorMainClase7 {
+
+	public static void main(String[] args) {
+		
+		//Para poder instaciar una clase fuera de su paquete
+		//
+		Buscador b = new Buscador();
+		
+		
+	}
+
+}
